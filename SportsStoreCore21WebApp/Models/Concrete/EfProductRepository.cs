cdd838138809a00fcece87a9c070f70a780afb4b
Maheshkumar <pn.maheshkumar@gmail.com>
2019-06-30 11:44:39 +0530

    EFCore

--- SportsStoreCore21WebApp/Models/Concrete/EfProductRepository.cs
+++ SportsStoreCore21WebApp/Models/Concrete/EfProductRepository.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
+
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SportsStoreCore21WebApp.Models.Abstract;
@@ -10,17 +13,20 @@ using SportsStoreCore21WebApp.Models.Entities;
 
 namespace SportsStoreCore21WebApp.Models.Concrete
 {
-  public class EfProductRepository : IProductRepository
+  public class EfProductRepository : IProductRepository, IDisposable
   {
     private SportsStoreDbContext _context;
     private readonly ILogger<EfProductRepository> _logger;
+    private readonly IConfiguration _configuration;
 
     public EfProductRepository(SportsStoreDbContext sportsStoreDbContext, ILogger<EfProductRepository> logger, IConfiguration configuration)
     {
       _context = sportsStoreDbContext;
       _logger = logger;
+      _configuration = configuration;
     }
 
+    #region IProductRepository Members
     public void ClearCache()
     {
       throw new NotImplementedException();
@@ -46,14 +52,55 @@ namespace SportsStoreCore21WebApp.Models.Concrete
       throw new NotImplementedException();
     }
 
-    public Task<List<Product>> GetAllProductsAsync()
+    public async Task<List<Product>> GetAllProductsAsync()
     {
-      throw new NotImplementedException();
+      try
+      {
+        var productsList = await _context.Products.ToListAsync();
+        _logger.LogInformation($"MN ---- ")
+      }
+      catch (Exception ex)
+      {
+
+        throw;
+      }
     }
 
     public Task UpdateAsync(Product product)
     {
       throw new NotImplementedException();
+    }
+
+
+    #endregion
+
+    #region IDispose Member
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    } 
+    #endregion
+
+    private void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        if (_context != null)
+        {
+          _context.Dispose();
+          _context = null;
+        }
+      }
+    }
+
+    private void LogInfo(string message)
+    {
+      _logger.LogInformation($"MN ---- {message}");
+    }
+    private void LogError(string message)
+    {
+      _logger.LogInformation($"MN ---- Error in --- {message}");
     }
   }
 }
